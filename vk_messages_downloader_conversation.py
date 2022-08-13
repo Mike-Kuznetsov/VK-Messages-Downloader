8f4bc02ff9b0809186ab2beac42a7a36a36fa5e4
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-08-13 20:27:51 +0300

    Update vk_messages_downloader_conversation.py

--- vk_messages_downloader_conversation.py
+++ vk_messages_downloader_conversation.py
@@ -6,6 +6,7 @@
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+session_api = vk_session.get_api();
 
 # Get your VK token from here https://vkhost.github.io/ and put it below:
 # Получите ВК токен здесь https://vkhost.github.io/ и вставьте его ниже (Берете из адресной строки):
@@ -16,9 +17,10 @@ chat_id=42;
 # Enter ID of your account:
 # Введите ID вашего аккаунта:
 user_id=171414378;
-
-session_api = vk_session.get_api();
+#You can change name of output file. If file exists, you can delete it, or change name, or change "x" to "w".
+#Вы можете поменять название файла, по умолчанию сообщения сохранятся в vk_messages.txt. Если файл уже существует, то вы можете удалить его, сменить название или поменять "x" на "w"
 f = open('new_vk_messages2.txt', 'x', encoding="utf-8");
+
 lastMessage = session_api.messages.getHistory(count=1, peer_id=2000000000+chat_id, user_id=user_id);
 pages=int(lastMessage['items'][0]['id']/200+1);
 print("Pages (page - 200 messages): " + str(pages));
