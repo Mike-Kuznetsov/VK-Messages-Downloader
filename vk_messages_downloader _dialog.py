0c926584880505ef83180733e12a41ac2f3fa78b
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2023-07-09 13:16:09 +0300

    Update vk_messages_downloader _dialog.py

--- vk_messages_downloader _dialog.py
+++ vk_messages_downloader _dialog.py
@@ -9,29 +9,29 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 
 # Get your VK token from here https://vkhost.github.io/ and put it below:
 # Получите ВК токен здесь https://vkhost.github.io/ и вставьте его ниже (Берете из адресной строки):
-vk_session = vk_api.VkApi(token="PASTE-YOUR-TOKEN-HERE");
-session_api = vk_session.get_api();
+vk_session = vk_api.VkApi(token="PASTE-YOUR-TOKEN-HERE")
+session_api = vk_session.get_api()
 # Put here ID of person you have conversation with:
 # Вставьте сюда ID пользователя с которым у вас диалог:
-user_id=171414378;
+user_id=171414378
 #You can change name of output file. If file exists, you can delete it, or change name, or change "x" to "w".
 #Вы можете поменять название файла, по умолчанию сообщения сохранятся в vk_messages.txt. Если файл уже существует, то вы можете удалить его, сменить название или поменять "x" на "w"
-f = open('vk_messages.txt', 'x', encoding="utf-8");
+f = open('vk_messages.txt', 'x', encoding="utf-8")
 
 #Let's get ID of the last message and count requests count, because we can ask only for 200 messages per request:
 #Получаем ID последнего сообщения и считаем количество запросов т.к. мы можем получить только 200 сообщений за запрос
-lastMessage = session_api.messages.getHistory(count=1, peer_id=user_id);
-requestCount=int(lastMessage['items'][0]['id']/200+1);
-print("RequestCount (request - 200 messages): " + str(requestCount));
+lastMessage = session_api.messages.getHistory(count=1, peer_id=user_id)
+requestCount=int(lastMessage['items'][0]['id']/200+1)
+print("RequestCount (request - 200 messages): " + str(requestCount))
 
 #And getting messages, 200 per request
 #И получаем сообщения по 200 штук за раз
 for i in range(1, requestCount+1):
-    history = session_api.messages.getHistory(count=200, peer_id=user_id, start_message_id=200*i);
-    history = history['items'];
+    history = session_api.messages.getHistory(count=200, peer_id=user_id, start_message_id=200*i)
+    history = history['items']
     for j in reversed(history):
-        f.write(str(j)+"\n");
-    print(str(i)+" out of "+str(requestCount));    
+        f.write(str(j)+"\n")
+    print(str(i)+" out of "+str(requestCount))  
     
-f.close();
-print("done");
+f.close()
+print("done")
